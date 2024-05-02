4004aafa9558bb7f4a4e2f6b416d56588d7f92a3
mathgod152 <matheus2002hbl@hotmail.com>
2024-05-02 20:13:56 -0300

    Exercice training Database

--- server/internal/entity/exerciceTrainingInterface.go
+++ server/internal/entity/exerciceTrainingInterface.go
@@ -1,6 +1,6 @@
 package entity
 
-type ExerciceTraining interface {
+type ExerciceTrainingInterface interface {
 	Create(exercise *TrainingExerciceEntity) (TrainingExerciceEntity, error)
 	FindAll(page, limit int, sort string) ([]*TrainingExerciceEntity, error)
 	FindByID(id string) (*TrainingExerciceEntity, error)
